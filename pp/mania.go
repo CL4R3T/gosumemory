4eedf56ac453fbd45539741284f2e1d4dd9d7bb9
helloender <helloender@126.com>
2024-08-15 20:17:09 +0800

    update: more useful log of pp calc

--- pp/mania.go
+++ pp/mania.go
@@ -7,8 +7,10 @@ import (
 
 func calculateManiaPP(stars float64, noteCount float64, hit300g int16, hit300 int16, hit200 int16, hit100 int16, hit50 int16) float64 {
 	accPP := (320*float64(hit300g)+300*float64(hit300)+200*float64(hit200)+100*float64(hit100)+50*float64(hit50))/(320*noteCount)
-	fmt.Println("300g=%d, 300=%d, 200=%d, 100=%d, 50=%d",hit300g,hit300,hit200,hit100,hit50)
-	return calculateManiaPPMax(stars,noteCount)*math.Max(accPP-0.8,0)*5
+	PPMax := calculateManiaPPMax(stars,noteCount)
+	PP := PPMax*math.Max(accPP-0.8,0)*5
+	fmt.Println(PPMax,PP,hit300g,hit300,hit200,hit100,hit50)
+	return PP
 }
 func calculateManiaPPMax(stars float64, noteCount float64) float64 {
 	return math.Pow(math.Max(stars-0.15,0.05),2.2)*(1+0.1*math.Min(1,noteCount/1500))*8
