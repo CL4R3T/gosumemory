79d91526611ed7c20be6b80e517ae3fd7bf52d6c
helloender <helloender@126.com>
2024-08-15 17:05:44 +0800

    fix: avoid negative pp

--- pp/mania.go
+++ pp/mania.go
@@ -5,8 +5,8 @@ import (
 )
 
 func calculateManiaPP(stars float64, noteCount float64, hit300g int16, hit300 int16, hit200 int16, hit100 int16, hit50 int16) float64 {
-	accPP := float64(320*hit300g+300*hit300+200*hit200+100*hit100+50*hit50)/(320*noteCount)
-	return calculateManiaPPMax(stars,noteCount)*(accPP-0.8)*5
+	accPP := (320*hit300g+300*hit300+200*hit200+100*hit100+50*hit50)/(320*noteCount)
+	return calculateManiaPPMax(stars,noteCount)*math.Max(accPP-0.8,0)*5
 }
 func calculateManiaPPMax(stars float64, noteCount float64) float64 {
 	return math.Pow(math.Max(stars-0.15,0.05),2.2)*(1+0.1*math.Min(1,noteCount/1500))*8
